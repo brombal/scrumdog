a31d8866fa86e7ccab50073c6edadf32855e3bbc
Alex Brombal <alex@brombal.com>
2020-07-05 19:00:42 -0500

    Update

--- server/src/html.ts
+++ server/src/html.ts
@@ -1,21 +1,19 @@
-import html from 'tagged-template-noop';
-
-export default 'asdf';
-/*
-export default html`
+export default `
   <!DOCTYPE html>
   <html>
     <head>
       <meta charset="utf-8" />
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
-      ${!process.env.LOCAL_FONTS
-        ? html`
+      ${
+        !process.env.LOCAL_FONTS
+          ? `
             <link
               href="https://fonts.googleapis.com/css2?family=Baloo+Chettan+2:wght@400;500;600;700;800&display=swap"
               rel="stylesheet"
             />
           `
-        : ""}
+          : ""
+      }
       <script>
         window.env = ${JSON.stringify(
           Object.keys(process.env)
@@ -40,4 +38,3 @@ export default html`
     </body>
   </html>
 `;
-*/
